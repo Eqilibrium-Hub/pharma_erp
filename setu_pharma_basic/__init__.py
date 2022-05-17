805c6d8dbb1d22aff4e2780e52372ec9054d2cb6
rahul-setu <rahul.setuconsulting@gmail.com>
2022-05-17 10:43:41 +0530

    [MOD]: Made Product Vairant Enable for All companies by default.

--- setu_pharma_basic/__init__.py
+++ setu_pharma_basic/__init__.py
@@ -1,3 +1,4 @@
+from odoo import api, SUPERUSER_ID
 from odoo.tools import convert_file
 from . import models
 from . import tools
@@ -17,6 +18,17 @@ def import_csv_data(cr, registry):
             kind='init'
         )
 
+def init_settings(env):
+    """ Make It Enable Product Variant By Default for the Products."""
+    for company in env['res.company'].search([]):
+        res_config_id = env['res.config.settings'].create({
+            'company_id': company.id,
+            'group_product_variant': True
+        })
+        # We need to call execute, otherwise the "implied_group" in fields are not processed.
+        res_config_id.execute()
 
 def post_init(cr, registry):
     import_csv_data(cr, registry)
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    init_settings(env)
