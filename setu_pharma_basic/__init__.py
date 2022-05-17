404f47c31db0bc5cb5a67ffb43ec3a77cea6c961
rahul-setu <rahul.setuconsulting@gmail.com>
2022-05-17 11:09:19 +0530

    [MOD]: Create 1 Fiscal Year Automatically.

--- setu_pharma_basic/__init__.py
+++ setu_pharma_basic/__init__.py
@@ -3,6 +3,7 @@ from odoo.tools import convert_file
 from . import models
 from . import tools
 from . import wizards
+import datetime
 
 
 def import_csv_data(cr, registry):
@@ -18,6 +19,7 @@ def import_csv_data(cr, registry):
             kind='init'
         )
 
+
 def init_settings(env):
     """ Make It Enable Product Variant By Default for the Products."""
     for company in env['res.company'].search([]):
@@ -28,7 +30,23 @@ def init_settings(env):
         # We need to call execute, otherwise the "implied_group" in fields are not processed.
         res_config_id.execute()
 
+
+def init_fiscal_years_and_periods(env):
+    """ Create Current Year's Fiscal Period By Defualt. """
+    fy = env['setu.pharma.fiscalyear'].create({
+        "end_month": '3',
+        "start_month": '4',
+        "end_year": str(datetime.datetime.now().year + 1),
+        "start_year": str(datetime.datetime.now().year),
+    })
+    # We need to call generate_fiscal_periods, otherwise the
+    # periods are not generated processed.
+    fy._onchange_name()
+    fy.generate_fiscal_period()
+
+
 def post_init(cr, registry):
     import_csv_data(cr, registry)
     env = api.Environment(cr, SUPERUSER_ID, {})
     init_settings(env)
+    init_fiscal_years_and_periods(env)
